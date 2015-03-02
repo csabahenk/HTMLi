18291a4d3e10b33272274684b4d68de475d29073
Csaba Henk <csaba@lowlife.hu>
2015-03-02 02:05:02 +0100

    allow multiple tree layouts
    - Instead of getting information about trees
      by direct index manipulation, encapsulate
      such info to an OO layer
    - Provide two implementations for this.
      "StandardLayout" and "InlineLayout"
    - Keep it internal and local, ie. instead
      of exchanging layout objects, the tree hierarcy
      is still kept internally (instead of recursive
      linked layout objects) and functions still
      work with a tree based interface
    - broaden scope for input/output formats

--- htmli.rb
+++ htmli.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+
 module HTMLi
 extend self
 
@@ -18,7 +19,7 @@ extend self
       tokens << if closing
         [:tagclose, tagname]
       else
-        [singleton ? :tagsingleton : :tagopen, tagname, attr]
+        [singleton ? :tagsingleton : :tagopen, tagname] + [attr].compact
       end
       doc = doc[fulltag.size..-1]
     end
@@ -48,44 +49,173 @@ extend self
     tokens
   end
 
-  def mktree tokens
-    tree = [[:root], []]
+  class LayoutBase
+
+    attr_reader :tree
+
+    def category
+      tree[0][0]
+    end
+
+    def tag
+      tree[0][1]
+    end
+
+    def attr
+      tree[0][2]
+    end
+
+    def self.makehead cat, tag, attr=nil
+      [cat, tag] + [attr].compact
+    end
+
+  end
+
+  class StandardLayout < LayoutBase
+
+    def initialize category, tag, attr:nil, content:[], height:nil
+      @tree = [self.class.makehead(category, tag, attr), content] + [height].compact
+    end
+
+    def content
+      tree[1]
+    end
+
+    def height
+      tree[2]
+    end
+
+    def height= n
+      tree[2] = n
+    end
+
+    def << e
+      content << e
+    end
+
+  end
+
+  class InlineLayout < LayoutBase
+
+    def initialize category, tag, attr:nil, content:[], height:nil
+      @tree = [self.class.makehead(category, tag, attr)] + content + [height].compact
+    end
+
+    def content
+      tree[1..-1].select { |e| [Array, String].include? e.class }
+    end
+
+    def height
+      tree[1..-1].find { |e| Integer === e }
+    end
+
+    def height= n
+      tree << n
+    end
+
+    def << e
+      tree << e
+    end
+
+  end
+
+  def findlayout tree=nil, layout: nil
+    layoutcls = case layout
+    when String, Symbol
+      HTMLi.const_get(layout.to_s.capitalize + "Layout")
+    when Class
+      layout
+    when nil
+      if LayoutBase === tree
+        tree.class
+      else
+        case tree.size
+        when 0
+          raise "invalid tree #{tree}"
+        when 1
+          InlineLayout
+        else
+          b = tree[1]
+          case b
+          when String,Integer
+            InlineLayout
+          when Array
+            case b[0]
+            # if b is Array, it's either the content container
+            # of StandardLayout or a tree that's part of inlined
+            # container of InlineLayout.
+            when nil,String,Integer
+              # b can't be tree, as trees start with an Array,
+              # so by exclusion we reach to StandardLayout
+              StandardLayout
+            when Array
+              # were b a tree, it's starting element's starting
+              # element should be a symbol/string; in the other
+              # case that's still an array
+              Array === b[0][0] ? StandardLayout : InlineLayout
+            else
+              raise "invalid tree #{tree}"
+            end
+          end
+        end
+      end
+    else
+      raise "unknown layout representation #{layout.class}"
+    end
+    if tree
+      if LayoutBase === tree
+        raise TypeError, "tree (#{tree.class}) does not match layout #{layout}" unless tree.class == layoutcls
+        return tree
+      end
+      layoutcls.allocate.instance_eval {
+        @tree = tree
+        self
+      }
+    else
+      layoutcls
+    end
+  end
+
+  def mktree tokens, layout:nil
+    _LO = findlayout layout: (layout||StandardLayout)
+    tree = _LO.new :root, nil
     cursor = [tree]
     tokens.each { |t|
       cat = t[0]
-      cont = t[1..-1]
+      payload = t[1..-1]
       if cat == :tagclose
-        raise "tag mismatch: #{cursor[-1][0]} closed by #{t}" unless cursor[-1][0][1..1] == cont
+        raise "tag mismatch: #{cursor[-1].tag} closed by #{t}" unless payload == [cursor[-1].tag]
         cursor.pop
       else
-        cursor[-1][1] << case cat
+        cursor[-1] << case cat
         when :str
-          t[1]
+          payload[0]
         when :tagsingleton, :tagvoid
-          [[cat.to_s.sub(/^tag/,"").to_sym] + cont, []]
+          _LO.new(cat.to_s.sub(/^tag/,"").to_sym, payload[0], attr: payload[1]).tree
         when :tagopen
-          cursor << [[:tag] + cont, []]
-          cursor[-1]
+          cursor << _LO.new(:tag, payload[0], attr: payload[1])
+          cursor[-1].tree
         end
       end
     }
     raise "non-sanitized tokens" unless cursor == [tree]
-    tree
+    tree.tree
   end
 
-  def calc_height tree, intra=false
+  def calc_height tree, intra=false, layout: nil
     return 0 if String === tree
+    tree = findlayout tree, layout: layout
     ha = []
-    tree[1].each { |t|
-      ha << calc_height(t, true)
+    tree.content.each { |t|
+      ha << calc_height(t, true, layout: layout)
     }
-    tree[2] = (ha.max||0) + 1
-    intra ? tree[2] : tree
+    tree.height = (ha.max||0) + 1
+    intra ? tree.height : tree.tree
   end
 
   def format tree, **opts
-    opts = {out: $>, separator: "\n", indent: "  ", collapse: 2, level: 0, context:{}}.merge opts
-    out,separator,collapse,context = opts.values_at(:out, :separator, :collapse, :context)
+    opts = {out: $>, separator: "\n", indent: "  ", collapse: 2, layout: nil, level: 0, context:{}}.merge opts
+    out,separator,collapse,layout,context = opts.values_at(:out, :separator, :collapse, :layout, :context)
     indent = opts[:indent] * opts[:level]
     case tree
     when String
@@ -95,24 +225,25 @@ extend self
         context[:lastchr] = l[-1]
       }
     when Array
-      cat = tree[0][0]
+      tree = findlayout tree, layout: layout
+      cat = tree.category
       indenting = proc { out << indent if context[:lastchr] == separator }
-      case cat
-      when :root
-        tree[1].each { |t| format t, **opts }
-      when :void, :singleton
+      case cat.to_s
+      when "root"
+        tree.content.each { |t| format t, **opts }
+      when "void", "singleton"
         indenting[]
-        out << "<#{tree[0][1..-1].compact.join(" ")}#{cat == :singleton ? "/" : ""}>"
+        out << "<#{[tree.tag, tree.attr].compact.join(" ")}#{cat == :singleton ? "/" : ""}>"
         context[:lastchr] = nil
-      when :tag
-        descending = proc { tree[1].each { |t| format t, **opts.merge(level: opts[:level]+1) } }
-        if tree[2] and tree[2] <= collapse
+      when "tag"
+        descending = proc { tree.content.each { |t| format t, **opts.merge(level: opts[:level]+1) } }
+        if tree.height and tree.height <= collapse
           indenting[]
-          out << "<#{tree[0][1..-1].compact.join(" ")}>"
+          out << "<#{[tree.tag, tree.attr].compact.join(" ")}>"
           context[:lastchr] = nil
           descending[]
           indenting[]
-          out << "</#{tree[0][1]}>"
+          out << "</#{tree.tag}>"
           context[:lastchr] = nil
         else
           if context.key? :lastchr and context[:lastchr] != separator
@@ -120,14 +251,15 @@ extend self
             context[:lastchr] = separator
           end
           indenting[]
-          out << "<#{tree[0][1..-1].compact.join(" ")}>" << separator
+          out << "<#{[tree.tag, tree.attr].compact.join(" ")}>" << separator
+          context[:lastchr] = separator
           descending[]
           unless context[:lastchr] == separator
             out << separator
             context[:lastchr] = separator
           end
           indenting[]
-          out << "</#{tree[0][1]}>" << separator
+          out << "</#{tree.tag}>" << separator
         end
       else
         raise "unknown token category #{cat}"
@@ -143,15 +275,47 @@ if __FILE__ == $0
   include HTMLi
 
   opts = {}
-  $*.each { |a|
-    if a =~ /\A(?:--)?([^=:]+)[=:](.*)/
-      opts[$1.to_sym] = begin
-        Integer $2
-      rescue ArgumentError
-        $2
+  args = []
+  while a = $*.shift
+    if a == "--"
+      break
+    elsif a =~ /\A(?:--)?([^=:]+)[=:](.*)/
+      o,v = $1,$2
+      opts[o.to_sym] = case v
+      when /\A\d+\Z/
+        Integer v
+      else
+        v
       end
+    else
+      args << a
     end
-  }
+  end
 
-  format calc_height(mktree(sanitize(tokenize(STDIN.read)))), **opts
+  $*.insert(0, args).flatten!
+
+  doc = $<.read
+  layout = opts.delete :layout
+  tree = begin
+    require 'json'
+    JSON.load doc
+  rescue
+    mktree sanitize(tokenize(doc)), layout: layout
+  end
+
+  case opts.delete :format
+  when "json"
+    require 'json'
+    puts tree.to_json
+  when "yaml"
+    require 'yaml'
+    puts tree.to_yaml
+  when %r{\A(json[:-_#@/.])?yajl\Z}
+    require 'yajl'
+    Yajl::Encoder.encode tree, $>
+  when "html", nil
+    format calc_height(tree), **opts
+  else
+    raise "unknown format #{format}"
+  end
 end
